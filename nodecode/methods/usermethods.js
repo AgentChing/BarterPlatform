d6dc607489c99e8dac4c39f9244ea649bbaa1c75
AgentChing <ashwin.ragenwar19@vit.edu>
2023-04-19 16:22:00 +0530

    Functionalised add user methode as well

--- nodecode/methods/usermethods.js
+++ nodecode/methods/usermethods.js
@@ -1,9 +1,35 @@
 exports.addtoInventory = addtoInventory;
-
+exports.addnewUser = addnewUser;
 const mongoose = require('mongoose');
 const express = require('express');
 const Product = require('../schema/product');
 const User = require('../schema/user');
+
+function addnewUser(userdata)
+{
+    const returnbody={message:"User added",returnstatus:"200",returnstatement:{}};
+    const idval = new mongoose.Types.ObjectId();
+    const user = new User({
+        _id: idval,
+        Name: userdata.name,
+        Password: userdata.password,
+        Email: userdata.email,
+        Address:userdata.address,
+        Inventory:userdata.inventory,
+        History:userdata.history,
+        Auction:userdata.auction,
+        Mybids:userdata.mybids
+    });
+    user.save().then(result=>{console.log(result);}).catch(err=>{
+        returnbody.message = 'Creating User Failed';
+        returnbody.returnstatus = 500;
+    });
+    if(returnbody.returnstatus === '200')
+    {
+        returnbody.returnstatement = idval;
+    }
+    return returnbody;
+}
 
 function addtoInventory(inventoryOwnerId,productId)
 {
--- nodecode/routes/processor.js
+++ nodecode/routes/processor.js
@@ -17,69 +17,18 @@ router.post('/',(req,res,next)=>{
     const action = req.body.action;
     if(action === 'addproduct')
     {
-
         const doc = productMethods.addProduct(req.body.target,req.body.data)
         res.status(parseInt(doc.returnstatus,10)).json({message:doc.message,data:doc.returnstatement});
-        
-
-        
-        /*
-        const data = req.body.data;
-        User.findById(req.body.target).exec().then(m=>{
-            const prod = new Product({
-                _id: new mongoose.Types.ObjectId(),
-                name: data.name,
-                price: data.price,
-                enlister: req.body.target,
-                description: data.description,
-                tags: data.tags,
-                intrestedtags:data.intrestedtags,
-                status:data.status
-            });
-
-            const err={addingproduct:"done",updatinginventory:"done"};
-            const returnstatus = 200;
-            prod.save().then(result=>{
-                User.findByIdAndUpdate(req.body.target,{$addToSet:{Inventory:prod._id}},{new:true, runValidators:true, returnDocument:true}).exec().then(doc=>{
-                    console.log(doc);
-                }).catch(err1=>{
-                    err.updatinginventory=err1;
-                    returnstatus=500;
-                })
-            }).catch(err2=>{
-                err.addingproduct=err2;
-                returnstatus=500;
-            })
-            res.status(returnstatus).json(err);
-            return;
-        }).catch(err=>{
-            console.log(err);
-            res.status(200).json({message:'no such user is present'});
-        })
-        */
     }
     
     else if(action === 'adduser')
     {
-        const data = req.body.data;
-        const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            Name: data.name,
-            Password: data.password,
-            Email: data.email,
-            Address:data.address,
-            Inventory:data.inventory,
-            History:data.history,
-            Auction:data.auction,
-            Mybids:data.mybids
-        
-        });
-        user.save().then(result=>{console.log(result);res.status(200).json({message:'user added',output:result});}).catch(err=>{
-            console.log(err);
-            res.status(500).json({error:err});
-        });
+        const doc = userMethods.addnewUser(req.body.data)
+        res.status(parseInt(doc.returnstatus,10)).json({message:doc.message,data:doc.returnstatement});
+   
     }
-    else{
+    else
+    {
         console.log(action+' is not a correct post action');
         res.status(500).json({message:action+' is not a correct post action'});
     }
